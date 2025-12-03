e4f1c6b38201d7286fa58fe23a2b9e7eaba4d6ca
Eric Snyder <ericdansnyder@gmail.com>
2025-12-03 12:54:00 +0100

    ios drafts working

---
+++ web-client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sudoduel.app',
+  appName: 'Sudoduel',
+  webDir: 'dist'
+};
+
+export default config;
