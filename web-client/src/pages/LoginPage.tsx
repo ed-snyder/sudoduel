73c005a24400d75665649fe69bb0632a33b22ef5
Eric Snyder <ericdansnyder@gmail.com>
2025-12-06 20:55:46 +0100

    Remove MatrixRain component from LoginPage

--- web-client/src/pages/LoginPage.tsx
+++ web-client/src/pages/LoginPage.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { useAuth } from '../context/AuthContext';
 import SudoDuelLogo from '../components/SudoDuelLogo';
-import MatrixRain from '../components/MatrixRain';
 
 export default function LoginPage() {
   const { login, signup } = useAuth();
@@ -32,11 +31,8 @@ export default function LoginPage() {
 
   return (
     <div className="min-h-screen bg-void flex flex-col">
-      {/* Matrix rain background */}
-      <MatrixRain />
-      
       {/* Content */}
-      <div className="flex-1 flex flex-col items-center justify-center px-4 safe-top safe-bottom relative z-10">
+      <div className="flex-1 flex flex-col items-center justify-center px-4 safe-top safe-bottom">
         {/* Logo */}
         <div className="mb-4">
           <SudoDuelLogo size="xl" />
