2af17a28a9ed5b32a4fd35126f5a4fa2c157ce25
Eric Snyder <ericdansnyder@gmail.com>
2025-12-08 00:42:25 +0100

    Move number pad and toolbar below grid

--- web-client/src/pages/GamePage.tsx
+++ web-client/src/pages/GamePage.tsx
@@ -1817,11 +1817,11 @@ export default function GamePage({ matchId, onGameEnd, onRematch, onFindNewMatch
         </div>
       </div>
       
-      {/* Spacer to maintain layout flow for other elements */}
-      <div className="flex-shrink-0" style={{ flex: '0 1 0', minHeight: '0' }}></div>
+      {/* Spacer to push number pad and toolbar below the grid */}
+      <div className="flex-shrink-0" style={{ minHeight: '50vh' }}></div>
 
-      {/* Number Pad - closer to grid */}
-      <div className="px-3 pt-1 pb-1" style={{ marginTop: '-60px' }}>
+      {/* Number Pad - below grid */}
+      <div className="px-3 pt-1 pb-1">
         <div className="grid grid-cols-9 gap-1.5 max-w-md mx-auto">
           {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((num) => {
             const count = digitCounts[num] || 0;
