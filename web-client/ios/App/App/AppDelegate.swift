13494fc0bec9c0dd467a0450cfaa408281af9fc9
Eric Snyder <ericdansnyder@gmail.com>
2025-12-12 11:35:56 +0100

    Fix AppDelegate: remove incorrect ApplicationDelegateProxy call for didFinishLaunchingWithOptions

--- web-client/ios/App/App/AppDelegate.swift
+++ web-client/ios/App/App/AppDelegate.swift
@@ -8,7 +8,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        return ApplicationDelegateProxy.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
+        return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
